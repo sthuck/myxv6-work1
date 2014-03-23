0a44879dd08a090b5ddb012f9760fb778afe9530
aviad <aviadhadad@gmail.com>
2014-03-24 01:36:44 +0200

    small change in wait()

--- proc.c
+++ proc.c
@@ -209,42 +209,8 @@ exit(void)
 int
 wait(void)
 {
-  struct proc *p;
-  int havekids, pid;
-
-  acquire(&ptable.lock);
-  for(;;){
-    // Scan through table looking for zombie children.
-    havekids = 0;
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->parent != proc)
-        continue;
-      havekids = 1;
-      if(p->state == ZOMBIE){
-        // Found one.
-        pid = p->pid;
-        kfree(p->kstack);
-        p->kstack = 0;
-        freevm(p->pgdir);
-        p->state = UNUSED;
-        p->pid = 0;
-        p->parent = 0;
-        p->name[0] = 0;
-        p->killed = 0;
-        release(&ptable.lock);
-        return pid;
-      }
-    }
-
-    // No point waiting if we don't have any children.
-    if(!havekids || proc->killed){
-      release(&ptable.lock);
-      return -1;
-    }
-
-    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
-    sleep(proc, &ptable.lock);  //DOC: wait-sleep
-  }
+  int a,b,c;
+  return wait2(&a,&b,&c);
 }
 
 int
@@ -274,7 +240,7 @@ wait2(int *wtime,int *rtime,int *iotime)
         p->killed = 0;
         *(rtime)=p->rtime;
         *(iotime)=p->iotime;
-        *(wtime)=p->etime-p->ctime-p->rtime-p->iotime;
+        *(wtime)=p->etime - p->ctime - p->rtime - p->iotime;
         release(&ptable.lock);
         return pid;
       }
